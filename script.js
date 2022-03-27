b10163b94c84aef3f2132b5b2a248fc076cd02ad
Daniel Pimenta <pmd2000123@gmail.com>
2022-03-27 14:09:22 +0100

    Finished etch a sketch

--- script.js
+++ script.js
@@ -7,9 +7,41 @@ function makeGrid(rows, cols) {
 
     for(let c = 0; c < (rows*cols); c++){
         let cell = document.createElement("div");
+        
         container.appendChild(cell).className = 'grid-item';
     }
+
+    
 
 }
 
 makeGrid(16, 16);
+
+
+function paintCells(){
+    let allCells = document.querySelectorAll('.grid-item');
+
+    for (let cell of allCells){
+        cell.addEventListener('mouseover', function(){
+            cell.style.backgroundColor = "blue";
+        });
+    }
+}
+
+
+paintCells();
+
+
+
+let clearButton = document.querySelector('#clear-grid');
+
+clearButton.addEventListener('click', function(){
+    let rows = window.prompt('Quantas linhas?');
+    let cols = window.prompt('Quantas colunas?');
+
+    container.innerHTML = "";
+
+    makeGrid(rows, cols);
+    paintCells();
+
+});
